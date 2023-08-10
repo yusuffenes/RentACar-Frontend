a2d12925abf82057e206af99d034d30329fced43
Yauxej <budaky4343@gmail.com>
2023-08-10 12:01:19 +0300

    10aug first Update

---
+++ src/app/components/car/car.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Car } from 'src/app/models/car';
+
+import { HttpClient } from '@angular/common/http';
+import { CarResponseModel } from 'src/app/models/carResponseModel';
+import { CarService } from 'src/app/services/car.service';
+
+@Component({
+  selector: 'app-car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
+})
+export class CarComponent implements OnInit {
+
+  dataLoaded = false;
+  cars:Car[] = [];
+
+
+
+  constructor(private carService:CarService){};
+
+  ngOnInit(): void {
+    this.getCars();
+    
+  }
+  getCars(){
+    this.carService.getCars().subscribe(response=>{
+      this.cars = response.data
+      this.dataLoaded = true;
+  })
+
+  }
+
+}
---
+++ src/app/services/car.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CarResponseModel } from '../models/carResponseModel';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarService {
+
+  apiUrl = "https://localhost:44390/api/Cars/getall"
+
+  constructor(private httpClient: HttpClient) { }
+
+  getCars():Observable<CarResponseModel>{
+    return this.httpClient.get<CarResponseModel>(this.apiUrl);
+  }
+
+}
