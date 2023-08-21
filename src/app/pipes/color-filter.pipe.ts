6749f68ac4bd72c3aaa987831dd19153df51f081
yusuffenes <budaky4343@gmail.com>
2023-08-21 15:50:25 +0300

    21first

---
+++ src/app/pipes/color-filter.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {Color} from "../models/color";
+import {filter} from "rxjs";
+
+@Pipe({
+  name: 'colorFilter'
+})
+export class ColorFilterPipe implements PipeTransform {
+
+  transform(value: Color[], filterText: string): Color[] {
+    filterText=filterText?filterText.toLocaleLowerCase():"";
+    return filterText?value.filter((c:Color)=>c.name.toLocaleLowerCase().indexOf(filterText)!==-1):value;
+  }
+
+}
